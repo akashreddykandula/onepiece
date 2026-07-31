81f32e7b855125a33e55de053c1b8fb7cb9c65c6
akashreddykandula <akashreddykandula@gmail.com>
2026-07-31 14:05:36 +0530

    Final realtime fixes

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -8,3 +8,15 @@ export const socket = io(SOCKET_URL, {
   withCredentials: true,
   autoConnect: true,
 });
+
+socket.on("connect", () => {
+  console.log("🟢 Socket connected:", socket.id);
+});
+
+socket.on("disconnect", (reason) => {
+  console.log("🔴 Socket disconnected:", reason);
+});
+
+socket.on("connect_error", (err) => {
+  console.error("❌ Socket connect error:", err.message);
+});
